c93ce8e510f4cb5ca5b93385e264b8098e0698d0
Baluvsg <balupandu@gmail.com>
2023-09-12 20:53:28 +0200

    All

---
+++ KMeans.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Sep  7 15:34:17 2023
+
+@author: BALU
+"""
+
+import pandas as pd
+
+home_data = pd.read_csv("D://Balu//Academic//Masters//Python//Project//Linkedin_data_engineer_profiles.csv")
+home_data.head()
